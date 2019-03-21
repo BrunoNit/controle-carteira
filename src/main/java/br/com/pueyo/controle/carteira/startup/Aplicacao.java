da9256a67071c573fd74b9599bb464d7503148d2
07669751770 <07669751770@serpro-1562381>
2019-03-21 09:30:16 -0300

    Commit inicial

---
+++ src/main/java/br/com/pueyo/controle/carteira/startup/Aplicacao.java
@@ -0,0 +1,14 @@
+package br.com.pueyo.controle.carteira.startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Aplicacao{
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aplicacao.class, args);
+    }
+
+}
